71818eaf3ea49b55cd4b2da7633453f4ec26f7d2
Khaled Damour <khaleddamour904@gmail.com>
2026-08-17 23:21:45 +0300

    Add static URL patterns for media and static files
    Added static URL patterns for MEDIA_URL and STATIC_URL.

--- yuksel_site/urls.py
+++ yuksel_site/urls.py
@@ -5,8 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('products.urls')), # أو اسم تطبيقك
+    path('', include('products.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# قم بإضافة هذين السطرين مباشرة هنا بدون شرط الـ DEBUG للتأكد من عملهما على السيرفر:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
